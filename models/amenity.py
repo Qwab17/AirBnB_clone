2056ecb41628ba2278752370c2299c0c9e8a2ee8
ASLawan <nawalnitsua@gmail.com>
2024-01-11 09:13:07 +0100

    Write class Amenity that inherits from BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Module with class Amenity that inherits from
+    BaseModel, base class
+
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class Amenity that inherits from BaseModel class"""
+    name = ""
