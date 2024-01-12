bbf5024315ac0463f2e8a7356074ac96e97cd556
ASLawan <nawalnitsua@gmail.com>
2024-01-12 23:12:02 +0100

    Update storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
 """
-    Module with class FileStorage that implements data
-    persistence by writing to files
+    Module with class FileStorage that implements
+    data persistence by writing to json file
 
 """
 import json
 
 
-class FileStorage:
-    """Class that serializesa and deserializes to/from json file"""
+class FileStorage():
+    """Class to serialize and deserialize to/from json file"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Method that returns the dictionary"""
+        """Method to return instance dictionary"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Method to set given obj to __objects"""
+        """Method to add key/value pair to dictionary"""
         key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serializes the dictionary (__objects) to JSON file"""
+        """Method to serialize dictionary to JSON file"""
         with open(FileStorage.__file_path, "w") as f:
             jn_dct = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(jn_dct, f)
 
     def reload(self):
-        """Deserialises the JSON file to dictionary"""
+        """Method to deserialize JSON file to dictionary"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -38,8 +38,8 @@ class FileStorage:
         from models.review import Review
         try:
             with open(FileStorage.__file_path, "r") as f:
-                json_dict = json.load(f)
-                for k, v in json_dict.items():
+                jsn_dct = json.load(f)
+                for k, v in jsn_dct.items():
                     cls_name = v["__class__"]
                     del v["__class__"]
                     FileStorage.__objects[k] = eval(cls_name)(**v)
