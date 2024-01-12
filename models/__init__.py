bc715187d2c389845271f41f692b140dc5364c15
ASLawan <nawalnitsua@gmail.com>
2024-01-12 23:08:07 +0100

    Update package

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-    Module to create unique file storage instance
-
-
+    Module to implement data persistence by
+    creating a unique filestorage from
+    FileStorage class
 """
 from models.engine.file_storage import FileStorage
 
