39b9b3fc13b17a35d6520332a9aa3d58e0afafef
ASLawan <nawalnitsua@gmail.com>
2024-01-12 23:08:44 +0100

    Update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-    Module with class Amenity that inherits from
-    BaseModel, base class
+    Module to implement class Amenity that
+    inherits from base class BaseModel
 
 """
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Class Amenity that inherits from BaseModel class"""
+    """Class Amenity to instantiate amenity instances"""
     name = ""
