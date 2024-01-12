6ebebd3f8a25e30d1b5d15361b03316aecfd75f9
ASLawan <nawalnitsua@gmail.com>
2024-01-12 23:10:28 +0100

    Update reviews

--- models/review.py
+++ models/review.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
     Module with class Review that inherits from
-    base class, BaseModel
+    base class BaseModel
 
 """
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Class Review that inherits from BaseModel base class"""
-    place_id = ""
-    user_id = ""
+    """Class Review to handle review instances"""
+    place_id = ""   # place.id
+    user_id = ""   # user.id
     text = ""
