fa58b7fa770cd14f22a71cdbda9024b8e2481a7d
ASLawan <nawalnitsua@gmail.com>
2024-01-13 20:24:45 +0100

    Add test cases for the class Review

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+    Module that implements test cases for
+    class Review
+
+"""
+import unittest
+from datetime import datetime
+from models.review import Review
+
+
+class TestReview(TestCase):
+    """Class implementing Review test cases"""
+    def test_instance(self):
+        """Test instance of attributes"""
+        review = Review()
+        self.assertIsInstance(review, Review)
+        self.assertIsInstance(review.place_id, str)
+        self.assertIsInstance(review.user_id, str)
+        self.assertIsInstance(review.text, str)
+        self.assertIsIntance(review.id, str)
+        self.assertIsInstance(review.created_at, datetime)
+        self.assertIsInstance(review.updated_at, datetime)
+
+    def test_attributes(self):
+        """Test class instance attributes"""
+        review = Review()
+        self.assertTrue(hasattr(review, 'place_id'))
+        self.assertTrue(hasattr(review, 'user_id'))
+        self.assertTrue(hasattr(review, 'text'))
+        self.assertTrue(hasattr(review, 'id'))
+        self.assertTrue(hasattr(review, 'created_at'))
+        self.assertTrue(hasattr(review, 'updated_at'))
+
+    def test_str_representation(self):
+        """Test the __str__ method on Review"""
+        review = Review()
+        expected_str = f"[Review] ({state.id}) {state.__dict__}"
+        self.assertEqual(review.__str__(), expected_str)
+
+
+if __name__ == "__main__":
+    unittest.main()
